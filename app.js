80f6c3217d313c73c85b0819fb6288e3c850e740
IDS Comercial <idscomercial@MacBook-Pro-de-Infraestructura.local>
2019-03-15 17:03:38 -0600

    implementacion de callback y datos actualizados en la tabla

--- app.js
+++ app.js
@@ -1,112 +1,78 @@
 
 let url = "http://127.0.0.1:8080/api/alumnos";
-var contactos = {};
 
+function guardarDatos(){
+    event.preventDefault();
 
-function obtenerDatos(){
-event.preventDefault();
-
-let jsonJ = {}
-
-   var nombre = document.getElementById("nombre").value;
-   telPersonal = document.getElementById("telPerson").value;
-   emailPerson = document.getElementById("emailPerson").value;
-    emailOfi = document.getElementById("emailOfi").value;
-    telOfi = document.getElementById("telOfi").value;
-    fechaCumpleaños = document.getElementById("fechaCumpleaños").value;
+    const nombre = document.getElementById("nombre").value;
+    const fechaCumpleaños = document.getElementById("fechaCumpleaños").value;
+    const telPerson = document.getElementById("telPerson").value;
+    const emailPerson = document.getElementById("emailPerson").value;
+    const emailOfi = document.getElementById("emailOfi").value;
+    const telOfi = document.getElementById("telOfi").value;
     
 
-    jsonJ = {
+    const jsonJ = {
         "nombre" : nombre,
-        "telPerson" : telPersonal,
+        "fechaCumpleaños" : fechaCumpleaños ,
+        "telPerson" : telPerson,
         "emailPerson"  : emailPerson,
         "emailOfi"   :  emailOfi,
-        "telOfi" : telOfi,
-        "fechaCumpleaños" : fechaCumpleaños ,
-        
+        "telOfi" : telOfi
     }
-
-    // jsonJ = {
-    //     "nombre" : nombre,
-    //     "edad"  : edad,
-    //      }
 
     mandarParametros(jsonJ)
-    getTablaDatos(jsonJ)
-    deleteUser(jsonJ.id)
 }
-    
-
-
-// getDatos();
 
 function mandarParametros(datos){
-   
-
-    nombre = document.getElementById("nombre").value
-    var json = JSON.stringify(datos);
-    
+    const nombre = datos.nombre
+    const json = JSON.stringify(datos);
     // instanciando el metodo XMLHttp
-    var request = new XMLHttpRequest();
-
+    const request = new XMLHttpRequest();
     // Solicitando el metodo post con la url de nuestro servicio y true quiere decir que sera una peticion asincrona
     request.open("POST", url, true);
-// seteando las cabeceras
+    // seteando las cabeceras
     request.setRequestHeader('Content-type','application/json; charset=utf-8');
     // cuando la peticion empiece 
-    request.onload = function () {
-        
-	if (request.readyState == 4 && request.status == "201") {
-		console.table(json);
-	} else {
-		console.error();
+    request.onload = function () {        
+        if (request.readyState == 4 && request.status == "200") {
+            alert(`se ha guardado correctamente el usuario ${nombre}`)
+            console.table(json);
+        } else {
+            console.error();
+        }
     }
 
-   
+    request.send(json);
 }
 
-request.send(json);
-    alert(`se ha guardado correctamente el usuario ${nombre}`)
+function getDatos (callback){
 
-}
+    var datos  = new XMLHttpRequest()
+    datos.open('GET', url, true)
+    // datos.open('GET', url+'/1', true)
 
-getDatos();
+    // funcion que se ejecuta al obtener los datos
+    datos.onload = function () {
 
-function getDatos (){
+        let users = JSON.parse(datos.responseText);
+        // getTablaDatos(users);
 
-var datos  = new XMLHttpRequest()
-datos.open('GET', url, true)
-// datos.open('GET', url+'/1', true)
-
-// funcion que se ejecuta al obtener los datos
-datos.onload = function () {
-
-    let users = JSON.parse(datos.responseText);
-    // getTablaDatos(users);
-
-	if (datos.readyState == 4 && datos.status == "200") {
-        console.table(users);
-        
-	} else {
-		console.error(users);
+        if (datos.readyState == 4 && datos.status == "200") {
+            console.table(users);
+            if (typeof(callback) === 'function') {
+                callback(users)
+            }
+            
+        } else {
+            console.error(users);
+        }
     }
-    contactos = users;
-    users.forEach(element => {
-            let lista = element
-            console.log(lista);
-            return lista;
-
-// 
-});
-
-
-
-}
-datos.send(null);
+    datos.send(null);
+    wewe
 }
 // Crea la tabla con los datos obtenidos
-function getTablaDatos(){
-    datos = contactos;
+function renderTabla(datos){
     // console.warn(datos);
     let tablastr = `<table id="tabladatos" class="table TablaDatos" width="100%" cellpadding="5" cellspacing="100%">
     <tr>
@@ -119,30 +85,28 @@ function getTablaDatos(){
 
     </tr>
     <tbody>`;
-datos.forEach(item => {
-    // console.log(item);
-tablastr =  tablastr + `<tr>`;
-for(prop in item){
-    if(prop !== "id"){
-        // console.log(item.id);
-        tablastr = tablastr + `<td> ${item[prop]} </td>`;
+    datos.forEach(item => {
+        // console.log(item);
+    tablastr =  tablastr + `<tr>`;
+    for(prop in item){
+        if(prop !== "id"){
+            tablastr = tablastr + `<td> ${item[prop]} </td>`;
+        }
     }
+    tablastr = tablastr + `<td>
+                            <p><button type="button"; "`+item.id+`" onclick="alert('quieres borrar a:')"; class="btn_azul">Editar</a>
+                            <p><button type="button"; "" onclick="deleteUser(${item.id})"; class="waves-effect waves-light btn-small">Borrado</a>
+                        </td>`;
+    tablastr = tablastr + `
+                        </tbody>
+                    </tr>`;
+    });
+
+    document.getElementById("datosTable").innerHTML = tablastr;
 }
-tablastr = tablastr + `<td>
-                        <p><button type="button"; "`+item.id+`" onclick="alert('quieres borrar a:')"; class="btn_azul">Editar</a>
-                        <p><button type="button"; "`+item.id+`" onclick="deleteUser(${item.id})"; class="btn_azul">Boorado Main</a>
-                        
-                        
-                    </td>`;
-tablastr = tablastr + `
-                    </tbody>
-                </tr>`;
-                // console.warn(tablastr);
-});
 
-
-document.getElementById("datosTable").innerHTML = tablastr;
-
+function getTablaDatos () {
+    getDatos(renderTabla);
 }
 
 // Delete a user
